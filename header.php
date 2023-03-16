ca86530164b85309c53c0bc5fe7a5385938b5096
JayHuijbregts05 <505564@vistacollege.nl>
2023-03-16 09:44:25 +0000

    nav change

--- header.php
+++ header.php
@@ -22,10 +22,10 @@
             <a class="nav-link" href="news.php">Link</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="contact.php">Disabled</a>
+            <a class="nav-link disabled" href="contact.php">Contact</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="contact.php">Contact</a>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
