39bd7748c6f00c0b99cfc2123ef0d894ac0e92ce
Parisxx <505825@vistacollege.nl>
2023-03-16 12:59:02 +0100

    f

--- header.php
+++ header.php
@@ -4,14 +4,14 @@
 <html> <link rel="stylesheet" href="css/styles_header.css"></html>
 
 	<header class="header">
-		<h1 class="logo"><a href="#">DigiWise</a></h1>
+		<h1 class="logo"><a href="index.php">DigiWise</a></h1>
       <ul class="main-nav">
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
+          <li><a href="story.php">Ons verhaal</a></li>
+          <li><a href="challenges.php">Challenges</a></li>
+          <li><a href="showcase.php">Showcase</a></li>
+          <li><a href="gallery.php">Gallerij</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <li><a href="news.php">Nieuws</a></li>
       </ul>
 
 	</header> 
