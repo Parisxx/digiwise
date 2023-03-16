e60524eb54c0d8b0554a637603040b133856a066
JayHuijbregts05 <505564@vistacollege.nl>
2023-03-16 09:45:25 +0000

    1 nav

--- header.php
+++ header.php
@@ -1,12 +1,7 @@
 <?php
 
 ?>
-<html>
-<a href="index.php" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-<a href="news.php" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">News</a>
-<a href="contact.php" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Contact</a>
-<a href="foto.php" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Foto Gallery</a>
- </html>  
+
 
  <nav class="navbar navbar-expand-md navbar-dark bg-success fixed-top">
       <a class="navbar-brand" href="#">Navbar</a>
