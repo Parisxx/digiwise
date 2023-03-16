4837a7b06bac7998b410d9557fc49018284720bb
Parisxx <505825@vistacollege.nl>
2023-03-16 09:33:30 +0100

    g

--- index.php
+++ index.php
@@ -15,6 +15,12 @@
 //your PHP code goes here
 ?>
  <b></b>
+ <ul>
+  <li><a href="default.asp">Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Contact</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul>
  <?php
 
  ?>
