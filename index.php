51b1579f98a0fdfbf6cf584dfbcb7bd7db8d668d
Parisxx <505825@vistacollege.nl>
2023-03-16 09:40:22 +0100

    paginas

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
 ?>
  <b></b>
  <ul>
-  <li><a href="default.asp">Home</a></li>
-  <li><a href="news.asp">News</a></li>
-  <li><a href="contact.asp">Contact</a></li>
-  <li><a href="about.asp">About</a></li>
+  <li><a href="home.php">Home</a></li>
+  <li><a href="news.php">News</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <li><a href="foto.php">Fotogallery</a></li>
 </ul>
  <?php
 
