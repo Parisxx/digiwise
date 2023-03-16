badc551a631277094c4ad6976ebbc2a6dbc41d39
JayHuijbregts05 <505564@vistacollege.nl>
2023-03-16 09:45:57 +0000

    change

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 
 
  <nav class="navbar navbar-expand-md navbar-dark bg-success fixed-top">
-      <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
