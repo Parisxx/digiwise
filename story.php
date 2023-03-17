aa3ff8df0c14b4201b919d4e2b5d05cc46c05664
Parisxx <505825@vistacollege.nl>
2023-03-17 11:27:40 +0100

    ons verhaal pagina

--- story.php
+++ story.php
@@ -13,5 +13,20 @@
 ?>
 <img class="picture" src="images/digiwise_2.png">
 
+
+<div class="line">
+<p  class="text_top"> ICT heeft een steeds grotere rol in onze samenleving.</p>
+</div>
+<p class="text_bottom"> 
+    IDe noodzakelijke ICT-vaardigheden en -kennis zijn onmisbaar 
+    om deel te blijven nemen aan de maatschappij. De digitalisering van de samenleving 
+    heeft grote impact op de leer- en werkprocessen en is nodig in vrijwel alle beroepen en functies, 
+    van de aannemer tot aan de zorgmedewerker.
+</p>
+<p class="text_bottom">
+    Met een teruglopende arbeidspopulatie in de ICT-sector 
+    heeft DigiWise als speerpunt Limburg economisch sterk en autonoom te laten zijn uiterlijk in 2024. 
+</p>
+
 </body>
 </html>
