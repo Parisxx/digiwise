60548627ed22c2e722b10ab5a86bbde322e99b0a
Tristan Offermans <210990@vistacollege.nl>
2023-03-23 09:40:12 +0100

    ff

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="css/styles_index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@500;700;800&display=swap" rel="stylesheet">
 </head>
 <body>
 <html> 
@@ -17,8 +17,7 @@
  include 'header.php';
 ?>
 <img class="blocks" src="images/block.png">
-<h1 class="title_1">Wij zijn klaar voor de</h1>
-<h1 class="title_2">toekomst! </h1>
+<h1 class="title_1">Wij zijn klaar voor de <br><span>toekomst!</span></h1>
 <p class="intro">
     Digitalisering ICT heeft een steeds grotere rol in de samenleving. 
     Daar waar de ICTer vroeger systeembeheer tot focus gebied had, 
