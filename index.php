786584dc22d2aeb80a937c63efa5ae9947e0269c
Tristan Offermans <210990@vistacollege.nl>
2023-03-23 09:41:15 +0100

    font

--- index.php
+++ index.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Digiwise</title>
     <link rel="stylesheet" href="css/styles_index.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@500;700;800&display=swap" rel="stylesheet">
 </head>
 <body>
 <html> 
