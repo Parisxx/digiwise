676dd78ecfe986cd72fa8207b2150868b0cc67cb
Tristan Offermans <210990@vistacollege.nl>
2023-03-24 09:54:37 +0100

    font

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Digiwise</title>
     <link rel="stylesheet" href="css/styles_index.css">
+    <?php include('loadfont.php')?>
 </head>
 <body>
 <html> 
