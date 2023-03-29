5664023d32c707db1bf158f9be8a05998f79199b
Parisxx <505825@vistacollege.nl>
2023-03-29 10:44:50 +0200

    fotos

--- gallery.php
+++ gallery.php
@@ -17,12 +17,12 @@
 <div class="div_images">
 <img class="image" src="images/digiwise_3.png">
 <img class="image" src="images/digiwise_4.png">
-<img class="image" src="images/digiwise_2.png">
-<img class="image" src="images/digiwise_2.png">
-<img class="image" src="images/digiwise_2.png">
-<img class="image" src="images/digiwise_2.png">
-<img class="image" src="images/digiwise_2.png">
-<img class="image" src="images/digiwise_2.png">
+<img class="image" src="images/digiwise_6.png">
+<img class="image" src="images/digiwise_7.png">
+<img class="image" src="images/digiwise_8.png">
+<img class="image" src="images/digiwise_9.png">
+<img class="image" src="images/digiwise_10.png">
+<img class="image" src="images/digiwise_11.png">
 </div>
 
 </body>
