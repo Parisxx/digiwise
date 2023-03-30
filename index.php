8eabbbf880ce098f785d9b4ad119d487cfe9ec4c
JayHuijbregts05 <505564@vistacollege.nl>
2023-03-30 13:40:29 +0100

    footer

--- index.php
+++ index.php
@@ -77,37 +77,11 @@
             </div>
         </div>
     </div>
-    <div class="footer_container">
-    <div class="footer">
-        <ul class="footer_links">
-            <h2>Digiwise</h2>
-        <hr>
-            <li>Contact opnemen</li>
-            <li>Documenten</li>
-            <li>Kennis Maken</li>
-            <li>Partners</li>
-        </ul>
-        <ul class="footer_links">
-            <h2>Informatie</h2>
-        <hr>
-            <li>Contact opnemen</li>
-            <li>Documenten</li>
-            <li>Kennis Maken</li>
-            <li>Partners</li>
-        </ul>
-        <ul class="footer_links">
-            <h2>Contact</h2>
-        <hr>
-            <li>Contact opnemen</li>
-            <li>Documenten</li>
-            <li>Kennis Maken</li>
-            <li>Partners</li>
-        </ul>
-        
-    </div>
 </div>
 </div>
-
+<?php
+ include 'footer.php';
+?>
  </body>
  </html>
 </body>
