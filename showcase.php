94247f12fd5529ebaae37a84264a9e451a27815f
Parisxx <505825@vistacollege.nl>
2023-03-30 14:20:32 +0200

    g

--- showcase.php
+++ showcase.php
@@ -14,13 +14,35 @@
 ?>
 <img class="blocks" src="images/block.png">
 
-<div class="showcase">
-<img class="picture" src="images/digiwise_5.jpg">
-<p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+<div>
+<img class="picture_1" src="images/digiwise_5.jpg">
+<p class="text_1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Cras a dictum ipsum. Pellentesque augue sapien, sodales sit amet 
+    libero tempus, blandit venenatis ipsum. Maecenas fermentum urna a 
+    mattis molestie. Sed vulputate laoreet massa a laoreet. Donec vel aliquam 
+    nibh, sit amet sodales tellus.</p>
+
+<p class="text_2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
     Cras a dictum ipsum. Pellentesque augue sapien, sodales sit amet 
     libero tempus, blandit venenatis ipsum. Maecenas fermentum urna a 
     mattis molestie. Sed vulputate laoreet massa a laoreet. Donec vel aliquam 
     nibh, sit amet sodales tellus.</p>
 </div>
+
+<div>
+<img class="picture_2" src="images/digiwise_5.jpg">
+<p class="text_3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Cras a dictum ipsum. Pellentesque augue sapien, sodales sit amet 
+    libero tempus, blandit venenatis ipsum. Maecenas fermentum urna a 
+    mattis molestie. Sed vulputate laoreet massa a laoreet. Donec vel aliquam 
+    nibh, sit amet sodales tellus.</p>
+    
+<p class="text_4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Cras a dictum ipsum. Pellentesque augue sapien, sodales sit amet 
+    libero tempus, blandit venenatis ipsum. Maecenas fermentum urna a 
+    mattis molestie. Sed vulputate laoreet massa a laoreet. Donec vel aliquam 
+    nibh, sit amet sodales tellus.</p>
+</div>
+
 </body>
 </html>
