7f0a408d16a08a2302fff5a994a2d292b015e0bb
Parisxx <505825@vistacollege.nl>
2023-03-30 15:41:09 +0200

    f

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@
   <input class="border" type="text" name="organisation"><br>
 
   <label>Onderwerp:</label><br>
-  <input type="text" class="subject" rows="5" required name="subject"></input><br>
+  <textarea type="text" class="subject" rows="5" required name="subject"></textarea><br>
 <br>
   <input class="button" name="submit" type="submit" value="Verstuur">
   
