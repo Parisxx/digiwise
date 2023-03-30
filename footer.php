314229f633e1fc731b4f683076d7eadbde87a169
JayHuijbregts05 <505564@vistacollege.nl>
2023-03-30 13:52:56 +0100

    da

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles_footer.css">
+</head>
 <body>
-    
 <div class="footer_container">
     <div class="footer">
         <ul class="footer_links">
@@ -26,6 +33,5 @@
             <li>Kennis Maken</li>
             <li>Partners</li>
         </ul>
-        
     </div>
 </body>
