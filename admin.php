169add8fe703b12e17f817be5956f5f404d40fc2
Parisxx <505825@vistacollege.nl>
2023-04-20 09:37:02 +0200

    admin

--- admin.php
+++ admin.php
@@ -4,21 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Digiwise | Admin inloggen</title>
+    <title>Digiwise | Admin</title>
     <link rel="stylesheet" href="css/styles_admin.css">
     <link rel="icon" type="image/x-icon" href="images/logo.png">
 </head>
 <body>
 
-<form action="admin.php" method="post" enctype="multipart/form-data">
-  <label>Gebruikersnaam:</label><br>
-  <input required type="text" name="username"><br>
-
-  <label>Wachtwoord:</label><br>
-  <input type="text" name="wachtwoord"><br>
-<br>
-  <input name="submit" type="submit" value="Verstuur">
-  
-</form>
+<!-- username in hoek -->
+<!-- nieuws verwijderen en toevoegen -->
+<!-- gallerij verwijderen en toevoegen -->
+<!-- contact formulier berichten kunnen zien en verwijderen -->
+<!-- challenges verwijderen en toevoegen -->
 </body>
 </html>
