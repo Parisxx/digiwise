53b191e9f2598a502955ec2cf03919fc473f269d
Parisxx <505825@vistacollege.nl>
2023-04-20 09:22:12 +0200

    admin

---
+++ admin_inlog.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digiwise</title>
+    <link rel="stylesheet" href="css/styles_index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;500;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.0/css/all.css">
+    <link rel="icon" type="image/x-icon" href="images/logo.png">
+</head>
+<html> 
+<body>
+ <?php
+ include 'header.php';
+?>
+
+<?php
+include 'footer.php'
+?>
+</body>
+</html>
